675fb9a4d44b7a8c7267672a26bc87fc3d8d9de9
Penghao Chen <PHMAN45@Penghaos-MacBook-Pro.local>
2017-02-01 01:26:56 +0800

    Adjust accordingly with introduction of new Formatter class

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -112,7 +112,7 @@ public class Main {
             storage.save(addressBook);
             return result;
         } catch (Exception e) {
-            ui.showToUser(e.getMessage());
+            ui.getFormatter().showToUser(e.getMessage());
             throw new RuntimeException(e);
         }
     }
